c28ff54e0ccaeb139d163d4ceda47f11457a498b
lkb_shidaiwanwang <shidaiwangwang_lkb@163.com>
2017-10-30 17:40:42 +0800

    初始化

---
+++ mvvm_10/mvvm_10/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mvvm_10
+//
+//  Created by Apple on 2017/10/30.
+//  Copyright © 2017年 李康滨,工作qq:1218773641. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
